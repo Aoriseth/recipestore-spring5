93172dd319d312b097ff3f21921c3f3ffab4b5fa
Lennart Cockx <Lennart.Cockx@faros.be>
2018-12-28 11:50:14 +0100

    Initial commit Added basic controller to show index page

---
+++ src/main/java/me/cockx/learn/recipestore/RecipeStoreApplication.java
@@ -0,0 +1,14 @@
+package me.cockx.learn.recipestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecipeStoreApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/me/cockx/learn/recipestore/RecipeStoreApplicationTests.java
@@ -0,0 +1,17 @@
+package me.cockx.learn.recipestore;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RecipeStoreApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
