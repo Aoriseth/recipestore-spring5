4b641e8a347a2d984f7cb83782bab8ff01160185
Lennart Cockx <lennart.cockx@gmail.com>
2019-01-07 11:32:19 +0100

    Added spring data find functions for Category and UOM and tested them in the controller Showcased Optionals from Spring Data

--- src/main/java/me/cockx/learn/recipestore/controllers/MainController.java
+++ src/main/java/me/cockx/learn/recipestore/controllers/MainController.java
@@ -1,13 +1,40 @@
 package me.cockx.learn.recipestore.controllers;
 
+import me.cockx.learn.recipestore.domain.Category;
+import me.cockx.learn.recipestore.domain.UnitOfMeasure;
+import me.cockx.learn.recipestore.repositories.CategoryRepository;
+import me.cockx.learn.recipestore.repositories.UnitOfMeasureRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Optional;
 
 @Controller
 public class MainController {
 
+    private CategoryRepository categoryRepository;
+    private UnitOfMeasureRepository uomRepository;
+
+    public MainController(CategoryRepository categoryRepository, UnitOfMeasureRepository uomRepository) {
+        this.categoryRepository = categoryRepository;
+        this.uomRepository = uomRepository;
+    }
+
+
+
+
     @GetMapping({"/","","index","index.html"})
     public String index(){
+
+        Optional<Category> cat = categoryRepository.findByDescription("American");
+        Optional<UnitOfMeasure> uom = uomRepository.findByUom("Pinch");
+
+        if(cat.isPresent()&&uom.isPresent()){
+            System.out.println("Category id is: " +cat.get().getId());
+            System.out.println("Uom id is: "+uom.get().getId());
+        }
+
+
         return "index";
     }
 }
--- src/main/java/me/cockx/learn/recipestore/repositories/CategoryRepository.java
+++ src/main/java/me/cockx/learn/recipestore/repositories/CategoryRepository.java
@@ -3,5 +3,10 @@ package me.cockx.learn.recipestore.repositories;
 import me.cockx.learn.recipestore.domain.Category;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface CategoryRepository extends CrudRepository<Category,Long> {
+
+    Optional<Category> findByDescription(String description);
+
 }
