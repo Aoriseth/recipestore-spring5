58e0841b2d43f2788e2f83e45de8b4a2cea331b2
Lennart Cockx <lennart.cockx@gmail.com>
2019-01-10 15:06:10 +0100

    Added a simple test for RecipeServiceImpl using Mockito Mocks Uses manual mockito initialisation

---
+++ src/test/java/me/cockx/learn/recipestore/services/RecipeServiceImplTest.java
@@ -0,0 +1,41 @@
+package me.cockx.learn.recipestore.services;
+
+import me.cockx.learn.recipestore.domain.Recipe;
+import me.cockx.learn.recipestore.repositories.RecipeRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+public class RecipeServiceImplTest {
+
+    private RecipeService recipeService;
+
+    @Mock
+    private RecipeRepository recipeRepository;
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        recipeService  = new RecipeServiceImpl(recipeRepository);
+    }
+
+    @Test
+    public void getRecipes() {
+        Recipe recipe = new Recipe();
+        HashSet<Recipe> recipeData = new HashSet<>();
+        recipeData.add(recipe);
+
+        when(recipeService.getRecipes()).thenReturn(recipeData);
+
+        Set<Recipe> recipes = recipeService.getRecipes();
+        assertEquals(1,recipes.size());
+        verify(recipeRepository,times(1)).findAll();
+    }
+}
